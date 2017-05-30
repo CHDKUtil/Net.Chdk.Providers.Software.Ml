bf1c8ee1e3f64a2e42f798dac6efde3db48d5f83
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 21:33:56 +0300

    Use DataProvider<T>

--- src/Net.Chdk.Providers.Software.Ml/MlSourceProvider.cs
+++ src/Net.Chdk.Providers.Software.Ml/MlSourceProvider.cs
@@ -1,9 +1,15 @@
-﻿using Net.Chdk.Providers.Software.Product;
+﻿using Microsoft.Extensions.Logging;
+using Net.Chdk.Providers.Software.Product;
 
 namespace Net.Chdk.Providers.Software.Ml
 {
     sealed class MlSourceProvider : ProductSourceProvider
     {
+        public MlSourceProvider(ILoggerFactory loggerFactory)
+            : base(loggerFactory.CreateLogger<MlSourceProvider>())
+        {
+        }
+
         protected override string CategoryName => "EOS";
         protected override string ProductName => "ML";
     }
